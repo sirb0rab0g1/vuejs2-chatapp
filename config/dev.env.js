e01b7baef5873a525db4c1d5a5fea985f73f6f9a
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-10-21 16:07:48 +0800

    oh yea

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:8000"',
-  WS_URL:'"ws://localhost:8000"',
+  API_URL: '"https://dashboard.heroku.com"',
+  WS_URL:'"ws://dashboard.heroku.com"',
 })
